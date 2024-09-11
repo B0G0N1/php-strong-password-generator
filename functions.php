b2fd0b43d34e7def3a93b3e599e3393dda62238b
B0G0N1 <michelebogoni2001@gmail.com>
2024-09-11 06:00:20 +0200

    [FEAT] Add Parameters

--- functions.php
+++ functions.php
@@ -1,20 +1,34 @@
 <?php
-// Funzione per generare una password casuale
-function generateRandomPassword($length) {
+// Funzione per generare una password casuale con parametri
+function generateRandomPassword($length, $characters, $allow_duplicates) {
     $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $numbers = '1234567890';
     $symbols = '!&%$@#^*(){}[]';
 
-    // Crea una stringa base di caratteri disponibili
-    $characters = $alphabet . $numbers . $symbols;
+    // Crea la stringa base di caratteri disponibili
+    $base_string = '';
+
+    if (in_array('L', $characters)) {
+        $base_string .= $alphabet;
+    }
+    if (in_array('N', $characters)) {
+        $base_string .= $numbers;
+    }
+    if (in_array('S', $characters)) {
+        $base_string .= $symbols;
+    }
 
     // Inizializza la password vuota
     $password = '';
 
     // Cicla per la lunghezza richiesta e aggiungi caratteri casuali alla password
-    for ($i = 0; $i < $length; $i++) {
-        $randomIndex = rand(0, strlen($characters) - 1);
-        $password .= $characters[$randomIndex];
+    while (strlen($password) < $length) {
+        $randomIndex = rand(0, strlen($base_string) - 1);
+        $char = $base_string[$randomIndex];
+
+        if ($allow_duplicates || !str_contains($password, $char)) {
+            $password .= $char;
+        }
     }
 
     return $password;
--- index.php
+++ index.php
@@ -4,7 +4,15 @@ include './functions.php';
 
 // Controlla se è stata richiesta una password
 if (isset($_GET['length']) && $_GET['length'] != '') {
-    $password = generateRandomPassword($_GET['length']);
+    $characters = [];
+
+    if (isset($_GET['letters'])) $characters[] = 'L';
+    if (isset($_GET['numbers'])) $characters[] = 'N';
+    if (isset($_GET['symbols'])) $characters[] = 'S';
+
+    $allow_duplicates = isset($_GET['duplicates']);
+
+    $password = generateRandomPassword($_GET['length'], $characters, $allow_duplicates);
     $_SESSION['password'] = $password;
     header('Location: success.php');
     exit();
@@ -24,6 +32,14 @@ if (isset($_GET['length']) && $_GET['length'] != '') {
     <form action="index.php" method="GET">
         <label for="length">Inserisci lunghezza password:</label>
         <input type="number" name="length" id="length" min="6" required>
+        <div>
+            <label><input type="checkbox" name="letters"> Lettere</label>
+            <label><input type="checkbox" name="numbers"> Numeri</label>
+            <label><input type="checkbox" name="symbols"> Simboli</label>
+        </div>
+        <div>
+            <label><input type="checkbox" name="duplicates"> Permetti duplicati</label>
+        </div>
         <button type="submit">Genera Password</button>
     </form>
 </body>
